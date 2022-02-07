782af284720e4e442e3cdaedc7d783209da0615b
duongductrong <duongductrong06@gmail.com>
2022-02-07 22:29:07 +0700

    Add html loading font for blog

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,12 @@ class MyDocument extends Document {
       <Html className="light">
         <Head />
 
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"></link>
+
         {/* <!-- Google Tag Manager --> */}
         <script
           dangerouslySetInnerHTML={{
