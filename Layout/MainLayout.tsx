823f7074aa6c16a33684f82f27489345cc56a6d4
duongductrong <duongductrong06@gmail.com>
2022-02-07 21:39:24 +0700

    Create Navbar with fixed item go home

--- Layout/MainLayout.tsx
+++ Layout/MainLayout.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
-// import Navbar from "../components/Navigate/Navbar";
+import Navbar from "../components/Navigate/Navbar";
 
 export interface MainLayoutProps {}
 
 const MainLayout: FC<MainLayoutProps> = (children) => {
   return (
     <section>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="container py-8">{children}</div>
     </section>
   );
